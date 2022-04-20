29f5aead75e71a6c7adf06a1143078e4f4aca835
ShreyaShristi <103538295+ShreyaShristi@users.noreply.github.com>
2022-04-21 03:46:23 +0530

    display picture update

--- content/about.js
+++ content/about.js
@@ -1,4 +1,4 @@
-export const image = "/assets/images/about/Display Picture.png";
+export const image = "/assets/images/about/Display picture.png";
 export const links = {
   tijori: "https://tijori.com",
   behance: "https://behance.com",
