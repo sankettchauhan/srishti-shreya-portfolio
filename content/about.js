05dd133a533b5df82b52b8a54f0b51f9433f8b4d
ShreyaShristi <103538295+ShreyaShristi@users.noreply.github.com>
2022-04-21 03:54:03 +0530

    updated behance link in about

--- content/about.js
+++ content/about.js
@@ -1,7 +1,7 @@
 export const image = "/assets/images/about/Display picture.png";
 export const links = {
   tijori: "https://tijori.com",
-  behance: "https://behance.com",
+  behance: "https://www.behance.net/shristishreya",
   resume: "resume.pdf",
 };
 export const coverImage = "/assets/images/about/Artwork.webp";
