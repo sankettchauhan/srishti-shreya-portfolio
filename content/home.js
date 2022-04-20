9048ad7bc152e4eac1da4f30338c17eb98d2e82d
ShreyaShristi <103538295+ShreyaShristi@users.noreply.github.com>
2022-04-21 03:44:15 +0530

    project 1 image update

--- content/home.js
+++ content/home.js
@@ -7,7 +7,7 @@ export const projects = [
     title: "project1",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam a, nostrum voluptatum eum sit expedita, consequatur minima omnis, autem perspiciatis rem? Quam perspiciatis cupiditate eum exercitationem, vitae eaque? A, adipisci.",
-    image: "/assets/images/phoen.webp",
+    image: "/assets/images/project1/P1DP.webp",
     bgcolor: "#000000",
   },
   {
