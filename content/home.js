a82bf0ca467df660b36e00bea00c253cebde326d
ShreyaShristi <103538295+ShreyaShristi@users.noreply.github.com>
2022-04-21 04:38:58 +0530

    Renamed

--- content/home.js
+++ content/home.js
@@ -4,14 +4,14 @@ export const description =
 
 export const projects = [
   {
-    title: "Clever Park",
+    title: "CLEVER PARK",
     description:
       "A digital parking solution that allows online and offline, Booking of parking slots, as well as providing navigation options for both offline and online users simultaneously.",
     image: "/assets/images/project1/P1DP.webp",
     bgcolor: "#000000",
   },
   {
-    title: "project2",
+    title: "MEDI TRIBE",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam a, nostrum voluptatum eum sit expedita, consequatur minima omnis, autem perspiciatis rem? Quam perspiciatis cupiditate eum exercitationem, vitae eaque? A, adipisci.",
     image: "/assets/images/phoen.webp",
