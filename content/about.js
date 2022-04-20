6c3abac38376950d25f08f046c05ddaecda0511a
ShreyaShristi <103538295+ShreyaShristi@users.noreply.github.com>
2022-04-21 03:37:53 +0530

    Display Picture update

--- content/about.js
+++ content/about.js
@@ -1,7 +1,8 @@
-export const image = "/assets/images/about/Display picture.png";
+export const image = "/assets/images/about/Display Picture.png";
 export const links = {
   tijori: "https://tijori.com",
   behance: "https://behance.com",
   resume: "resume.pdf",
 };
 export const coverImage = "/assets/images/phoen.webp";
+
