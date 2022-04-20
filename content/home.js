ce76584c3d434dff8381cff5348701ad4bc7d0e5
ShreyaShristi <103538295+ShreyaShristi@users.noreply.github.com>
2022-04-21 04:31:14 +0530

    project 3 in home added

--- content/home.js
+++ content/home.js
@@ -18,3 +18,8 @@ export const projects = [
     bgcolor: "#000000",
   },
 ];
+title: "project3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam a, nostrum voluptatum eum sit expedita, consequatur minima omnis, autem perspiciatis rem? Quam perspiciatis cupiditate eum exercitationem, vitae eaque? A, adipisci.",
+    image: "/assets/images/phoen.webp",
+    bgcolor: "#000000",
