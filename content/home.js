d3743039761c45082bf274e08037fe032ae340b5
ShreyaShristi <103538295+ShreyaShristi@users.noreply.github.com>
2022-04-21 04:06:21 +0530

    project 1 content update

--- content/home.js
+++ content/home.js
@@ -6,7 +6,7 @@ export const projects = [
   {
     title: "project1",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam a, nostrum voluptatum eum sit expedita, consequatur minima omnis, autem perspiciatis rem? Quam perspiciatis cupiditate eum exercitationem, vitae eaque? A, adipisci.",
+      "A Design Solution that allows online and offline, Booking of parking slots, as well as providing navigation options for both offline and online users simultaneously.",
     image: "/assets/images/project1/P1DP.webp",
     bgcolor: "#000000",
   },
